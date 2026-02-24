990933a65d368e7e1313a5a39bd6614ed9e020eb
unknown <alexandr.safonov22@gmail.com>
2026-02-24 18:26:02 +0500

    Album class created

---
+++ backend/Models/Album.cs
@@ -0,0 +1,9 @@
+﻿public class Album
+{
+    public int id {  get; set; }
+    public string name { get; set; } = "Name";
+    public int artistId { get; set; }
+    public string AlbumPicture { get; set; } = "img.png";
+    public Artist? Artist { get; set; }
+
+}
