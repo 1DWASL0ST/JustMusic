6695bd65067bc4f2cd095bdd48a51bc665b6f169
unknown <alexandr.safonov22@gmail.com>
2026-02-24 18:37:58 +0500

    track class created

--- backend/obj/Release/net8.0/backendAPI.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/backendAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backendAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00b48b545f3214f9e74b1d1fd99ca5a52b1ebc9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac99ba37fda6d6ce1657cd6a44d8dcc7d84f6c60")]
 [assembly: System.Reflection.AssemblyProductAttribute("backendAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backendAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
