54946392682b46aae1813f66ff29ae7384adc0c7
unknown <alexandr.safonov22@gmail.com>
2026-03-02 15:10:11 +0500

    UserResponse dto created

---
+++ backend/DTO/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace backendAPI.DTO
+{
+    public class UserResponse
+    {
+        public required string IDUser {  get; set; }
+        public required string UserName { get; set; }
+    }
+}
