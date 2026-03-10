163a2ef30d4b005ad01ab10032cf37a708a76f62
unknown <alexandr.safonov22@gmail.com>
2026-03-10 16:10:28 +0500

    album api shell created

---
+++ backend/Controllers/AlbumController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backendAPI.Data;
+
+namespace backendAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumController : ControllerBase
+    {
+        private readonly DataDbContext _dbcontext;
+
+        public AlbumController(DataDbContext context)
+        {
+            _dbcontext = context;
+        }
+
+        // GET: api/Album
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Album>>> GetAlbums()
+        {
+            return await _dbcontext.Albums.ToListAsync();
+        }
+
+        // GET: api/Album/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Album>> GetAlbum(int id)
+        {
+            var album = await _dbcontext.Albums.FindAsync(id);
+
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            return album;
+        }
+
+        // PUT: api/Album/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAlbum(int id, Album album)
+        {
+            if (id != album.IDAlbum)
+            {
+                return BadRequest();
+            }
+
+            _dbcontext.Entry(album).State = EntityState.Modified;
+
+            try
+            {
+                await _dbcontext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlbumExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Album
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Album>> PostAlbum(Album album)
+        {
+            _dbcontext.Albums.Add(album);
+            await _dbcontext.SaveChangesAsync();
+
+            return CreatedAtAction("GetAlbum", new { id = album.IDAlbum }, album);
+        }
+
+        // DELETE: api/Album/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAlbum(int id)
+        {
+            var album = await _dbcontext.Albums.FindAsync(id);
+            if (album == null)
+            {
+                return NotFound();
+            }
+
+            _dbcontext.Albums.Remove(album);
+            await _dbcontext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AlbumExists(int id)
+        {
+            return _dbcontext.Albums.Any(e => e.IDAlbum == id);
+        }
+    }
+}
