7b9572fb9d424ac8fc56f90a95ed26f04fba86f9
unknown <alexandr.safonov22@gmail.com>
2026-05-15 03:05:02 +0500

    админ панель готова

---
+++ backend/DTO/AlbumUpload.cs
@@ -0,0 +1,8 @@
+﻿namespace backendAPI.DTO
+{
+    public class AlbumUpload
+    {
+        public required int IDAlbum { get; set; }
+        public required IFormFile PictureFile { get; set; }
+    }
+}
---
+++ backend/DTO/TrackUpload.cs
@@ -0,0 +1,8 @@
+﻿namespace backendAPI.DTO
+{
+    public class TrackUpload
+    {
+        public required int IDSong { get; set; }
+        public required IFormFile AudioFile { get; set; }
+    }
+}
